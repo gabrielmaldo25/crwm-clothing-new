31cc550d76704bc8d1df9b43840db12e9a1af8d1
Gabriel <maldoangel@hotmail.com>
2024-01-22 21:00:04 -0300

    separate the category preview from the category itself for shop

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,13 @@
-import { Fragment, useContext } from "react";
-import { CategoriesContext } from "../../context/categories.context";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 import "./shop.styles.scss";
-import ProductCard from "../../components/product-card/product-card.component";
+import Category from "../category/category.component";
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h1>{title} </h1>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
