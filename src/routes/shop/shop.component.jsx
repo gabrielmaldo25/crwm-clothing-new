63a62db5fb154828bdd33884e500a548cbbc1381
Gabriel <maldoangel@hotmail.com>
2024-01-31 22:06:24 -0300

    update category selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,13 +5,13 @@ import Category from "../category/category.component";
 import { useEffect } from "react";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { useDispatch } from "react-redux";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments();
+      dispatch(setCategories(categoriesArray));
     };
     getCategoriesMap();
   }, []);
